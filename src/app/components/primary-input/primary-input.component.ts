72701722a25028ef8323eaeadd0e8f732049908a
Viviane <vivianeaguiarc@outlook.com>
2025-03-02 14:50:40 -0300

    feat: primary-input and login

---
+++ src/app/components/primary-input/primary-input.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+type InputTypes = "text" | "email" | "password"
+
+@Component({
+  selector: 'app-primary-input',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './primary-input.component.html',
+  styleUrl: './primary-input.component.scss'
+})
+export class PrimaryInputComponent  {
+  @Input() type: InputTypes = "text";
+  @Input() forName: string = "";
+  @Input() placeholder: string = "";
+  @Input() label: string = "";
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'] // Corrigido aqui
 })
+
 export class LoginComponent {
   loginForm!: FormGroup;
   constructor() {
